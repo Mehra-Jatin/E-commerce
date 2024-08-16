62d470c0557071f9298e97165b495cdbdc184baa
Mehra-Jatin <jatinmehra578@gmail.com>
2024-08-16 23:03:51 +0530

    navbar cart count

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -15,8 +15,9 @@ const Navbar = () => {
           <li className='cursor-pointer text-[20px] gap-[50px] text-[20px] hover:text-[18px] hover:border-b-2 border-red-600 '>Kids</li>
         </ul>
        <div className='flex justify-center items-center '>
-          <button className='px-[20px] py-[3px] border-2 border-black-500 h-[30px]'>Login</button>
+          <button className='px-[20px] py-[3px] border-2 border-black-500 h-[30px] rounded-[10px]'>Login</button>
        <img  className=' h-[30px] px-[20px]' src={cart_icon} alt='cart'  />
+       <div className='h-[20px] w-[20px] border-2 rounded-[9px] flex justify-center items-center bg-red-600 text-white ml-[-28px] mt-[-13px]'>0</div>
        </div>
     </div>
   )
